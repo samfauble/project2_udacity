86c6d4ae68209848e87a0fa06a2f45f5ce580769
Sam <sam@blackarcs.org>
2020-11-25 23:58:45 -0400

    commit before github

---
+++ README.md
@@ -0,0 +1,3 @@
+token name: "SparklyStars"
+token symbol: "SS"
+token address: 0x8FD97E4fE7bF48a60c2c6B94AC36868C2a89796c
--- truffle-config.js
+++ truffle-config.js
@@ -1,29 +1,14 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+let seed = "";
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
+let provider = () => {
+    return new HDWalletProvider({
+    mnemonic: {
+      phrase: seed
+    },
+    providerOrUrl: "https://rinkeby.infura.io/v3/KEY"
+  });
+}
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -48,9 +33,17 @@ module.exports = {
     //  network_id: "*",       // Any network (default: none)
     // },
 
+    rinkeby: {
+      provider,
+       network_id: "4",
+       gas: 4500000,
+       gasPrice: 10000000000,
+       networkCheckTimeout: 6000
+    },
+
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 9545,            // Standard Ethereum port (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
     
